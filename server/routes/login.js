f69937e991521b7d5193abd1d0ec8f9fc51170cc
JuliaGrisales <jgrisales@gmail.com>
2020-07-07 11:19:59 +0200

    User login

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,8 +4,40 @@ const User = require("../models/user")
 const app = express()
 
 app.post("/login", (req, res) => {
-  res.json({
-    ok: true
+  let body = req.body
+  User.findOne({ email: body.email }, (err, userDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      })
+    }
+
+    if (!userDB) {
+      console.log("User invalid")
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "User or password invalids"
+        }
+      })
+    }
+
+    if (!bcrypt.compareSync(body.password, userDB.password)) {
+      console.log("Password invalid")
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "User or password invalids"
+        }
+      })
+    }
+
+    res.json({
+      ok: true,
+      user: userDB,
+      token: 123
+    })
   })
 })
 
