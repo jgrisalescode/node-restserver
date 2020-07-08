31770880ebf305d1d22965a84cf31e482be3a0c3
JuliaGrisales <jgrisales@gmail.com>
2020-07-08 11:03:37 +0200

    API Key y API Secret of Google

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("./config/config")
 const express = require("express")
 const mongoose = require("mongoose")
+const path = require("path") // Needed for enable public folder
 const colors = require("colors")
 const app = express()
 const bodyParser = require("body-parser")
@@ -10,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// Enable public folder
+app.use(express.static(path.resolve(__dirname, "../public")))
 
 // Global config of routes
 app.use(require("./routes/index"))
